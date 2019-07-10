95d2792d1c88cd0fc770076d9e375570246c93e7
Constantin Roganov <rccbox@gmail.com>
2019-07-10 23:36:00 +0300

    [added] Docstrings [changed] connectX() methods refactored to pass through low level exceptions

--- src/main/kotlin/com/github/brake/smart_card/dsl/Reader.kt
+++ src/main/kotlin/com/github/brake/smart_card/dsl/Reader.kt
@@ -27,59 +27,103 @@ enum class Protocol(val protocol: String) {
 }
 
 /**
- * Connect to [Card] using given protocol and run [block] in context of implicitly opened basic [CardChannel]
- * @return null on success and thrown [SmartCardException] in case of error
+ * Connect to [Card] using given protocol and run [block] in context of implicitly opened basic [CardChannel].
+ *
+ * Usually [block] will construct APDUs and transmit them to the card through [CardChannel]
+ *
+ * @throws CardNotPresentException - if no card is present in this terminal
+ * @throws CardException - if a connection could not be established using the specified protocol,
+ *      if a connection has previously been established using a different protocol
+ *      or if the card operation failed
+ * @throws SecurityException - if a SecurityManager exists and the caller does not have the required permission
+ * @throws IllegalStateException - if this channel has been closed or if the corresponding Card has been disconnected
+ * @throws IllegalArgumentException - if the APDU encodes a MANAGE CHANNEL command
  *
  * Example:
  * ```kotlin
- *      reader.connectT0 {
- *          transmit {
- *              apdu { ... }
- *          }.withResult {
- *              // do something in context of ResponseAPDU
+ *      try {
+ *          reader.connectT0 {
+ *              transmit {
+ *                  apdu { ... }
+ *              }.withResult {
+ *                  // do something in context of ResponseAPDU
+ *              }
  *          }
- *      }?.let {
- *          // do something with Exception thrown
- *      }
+ *       }  catch(e: Exception) {
+ *           // do something with Exception
+ *       }
  * ```
  */
-fun CardTerminal.connect(how: Protocol, block: CardChannel.() -> Unit): SmartCardException? =
-    try {
-        var result: SmartCardException? = null
+fun CardTerminal.connect(how: Protocol, block: CardChannel.() -> Unit) {
+    connect(how.protocol).invoke(block)
+}
 
-        val card = connect(how.protocol)
-        card {
-            try {
-                block()
-            } catch (e: Exception) {
-                result = TransmitAPDUException(e)
-            }
-        }
-        result
-    } catch (e: Exception) {
-        ReaderConnectException(e)
-    }
+/**
+ * Connect to [Card] using protocol T0 and run [block] in context of implicitly opened basic [CardChannel].
+ *
+ * Usually [block] will construct APDUs and transmit them to the card through [CardChannel]
+ *
+ * @throws CardNotPresentException - if no card is present in this terminal
+ * @throws CardException - if a connection could not be established using the specified protocol,
+ *      if a connection has previously been established using a different protocol
+ *      or if the card operation failed
+ * @throws SecurityException - if a SecurityManager exists and the caller does not have the required permission
+ * @throws IllegalStateException - if this channel has been closed or if the corresponding Card has been disconnected
+ * @throws IllegalArgumentException - if the APDU encodes a MANAGE CHANNEL command
+ */
+fun CardTerminal.connectT0(block: CardChannel.() -> Unit) {
+    connect(Protocol.T0, block)
+}
 
-fun CardTerminal.connectT0(block: CardChannel.() -> Unit): SmartCardException? = connect(Protocol.T0, block)
+/**
+ * Connect to [Card] using protocol T1 and run [block] in context of implicitly opened basic [CardChannel].
+ *
+ * Usually [block] will construct APDUs and transmit them to the card through [CardChannel]
+ *
+ * @throws CardNotPresentException - if no card is present in this terminal
+ * @throws CardException - if a connection could not be established using the specified protocol,
+ *      if a connection has previously been established using a different protocol
+ *      or if the card operation failed
+ * @throws SecurityException - if a SecurityManager exists and the caller does not have the required permission
+ * @throws IllegalStateException - if this channel has been closed or if the corresponding Card has been disconnected
+ * @throws IllegalArgumentException - if the APDU encodes a MANAGE CHANNEL command
+ */
+fun CardTerminal.connectT1(block: CardChannel.() -> Unit) {
+    connect(Protocol.T1, block)
+}
 
-fun CardTerminal.connectT1(block: CardChannel.() -> Unit): SmartCardException? = connect(Protocol.T1, block)
+/**
+ * Connect to [Card] using any supported protocol and run [block] in context of implicitly opened basic [CardChannel].
+ *
+ * Usually [block] will construct APDUs and transmit them to the card through [CardChannel]
+ *
+ * @throws CardNotPresentException - if no card is present in this terminal
+ * @throws CardException - if a connection could not be established using the specified protocol,
+ *      if a connection has previously been established using a different protocol
+ *      or if the card operation failed
+ * @throws SecurityException - if a SecurityManager exists and the caller does not have the required permission
+ * @throws IllegalStateException - if this channel has been closed or if the corresponding Card has been disconnected
+ * @throws IllegalArgumentException - if the APDU encodes a MANAGE CHANNEL command
+ */
+fun CardTerminal.connectAuto(block: CardChannel.() -> Unit) {
+    connect(Protocol.AUTO, block)
+}
 
-fun CardTerminal.connectAuto(block: CardChannel.() -> Unit): SmartCardException? = connect(Protocol.AUTO, block)
-
-fun CardTerminal.waitForCardPresent(): Boolean = waitForCardPresent(0)
-
-fun CardTerminal.waitForCardAbsent(): Boolean = waitForCardAbsent(0)
-
+/** Return [List] of [CardTerminal] objects having given [State] (by default returns all available readers) */
 fun readers(state: State = State.ALL): List<CardTerminal> =
         TerminalFactory
             .getDefault()
             .terminals()
             .list(state)
 
+/** Return [List] of [CardTerminal] objects having [State.CARD_ABSENT] */
 fun readersNoCard(): List<CardTerminal> = readers(State.CARD_ABSENT)
 
+/** Return [List] of [CardTerminal] objects having given [State.CARD_INSERTION] */
 fun readersInsertion(): List<CardTerminal> = readers(State.CARD_INSERTION)
 
+/** Return [List] of [CardTerminal] objects having given [State.CARD_REMOVAL] */
 fun readersRemoval(): List<CardTerminal> = readers(State.CARD_REMOVAL)
 
+/** Return [List] of [CardTerminal] objects having given [State.CARD_PRESENT] */
 fun readersWithCard(): List<CardTerminal> = readers(State.CARD_PRESENT)
